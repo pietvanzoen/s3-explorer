fc55c8b6ec010a6b21c1970dc439e2b0b0988736
pietvanzoen <hi@pietvanzoen.com>
2020-10-31 15:29:52 +0100

    setup eslint and prettier

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-import { createServer, IncomingMessage, ServerResponse } from 'http';
+import { createServer, ServerResponse } from 'http';
 
 const { PORT = 8080 } = process.env;
 
-const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
+const server = createServer((_, res: ServerResponse) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('Hello World');
   res.end();
 });
